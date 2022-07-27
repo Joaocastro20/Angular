aebcaf05873137693a691087104a73fc1c1194b1
Joaocastro20 <joaopedroabreucastro@hotmail.com>
2022-07-27 17:17:26 -0300

    adicionar erro de validacao

--- formularios/src/app/template-formulario/template-formulario.component.ts
+++ formularios/src/app/template-formulario/template-formulario.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormularioComponent implements OnInit {
 
+  usuario:any = {
+    nome: '',
+    email: ''
+  }
+
   onSubmit(form:any){
     console.log(form)
   }
