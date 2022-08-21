ab0e19cbf0a78cac1939c958460a2acb51a3d905
Joaocastro20 <joaopedroabreucastro@hotmail.com>
2022-08-21 18:52:00 -0300

    adicionar post data-driven

--- formularios/src/app/data-formulario/data-formulario.component.ts
+++ formularios/src/app/data-formulario/data-formulario.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
@@ -10,7 +11,8 @@ export class DataFormularioComponent implements OnInit {
 
   formulario!: FormGroup;
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder,
+    private http: HttpClient) { }
 
   ngOnInit(): void {
     // primeira forma de se contruir o formulario
@@ -22,6 +24,11 @@ export class DataFormularioComponent implements OnInit {
       nome: [null],
       email: [null]
     })
+  }
+
+  onSubmit(){
+    this.http.post('https://httpbin.org/post',JSON.stringify(this.formulario.value)).subscribe(resposta => console.log(resposta));
+    console.log(this.formulario.value)
   }
 
 }
