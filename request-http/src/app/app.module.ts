0e564795964caa63fb2e834549c06ed76d0a487d
Joaocastro20 <joaopedroabreucastro@hotmail.com>
2022-09-05 16:35:02 -0300

    adicionar lista de cursos/service

--- request-http/src/app/app.module.ts
+++ request-http/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -12,7 +13,8 @@ import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- request-http/src/app/cursos-lista/cursos-lista.component.ts
+++ request-http/src/app/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosServiceService } from '../cursos-service.service';
+import { Curso } from '../models/curso';
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces:true
 })
 export class CursosListaComponent implements OnInit {
 
-  constructor() { }
+  cursos!:Curso[];
+
+  constructor(private service: CursosServiceService) { }
 
   ngOnInit(): void {
+    this.service.list().subscribe(dados => this.cursos = dados);
+  }
+
+  onRefresh(){
+
+  }
+
+  onEdit(curso:any){
+
+  }
+
+  onDelete(curso:any){
+
+  }
+
+  onConfirmDelete(){
+
+  }
+
+  onDeclineDelete(){
+
   }
 
 }
