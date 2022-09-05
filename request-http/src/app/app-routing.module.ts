56e91fee5d37c3df0f618c820b2dc431bf857d5c
Joaocastro20 <joaopedroabreucastro@hotmail.com>
2022-09-05 13:41:52 -0300

    adicionar bootstrap e configuracao inicial das rotas

--- request-http/src/app/app-routing.module.ts
+++ request-http/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',component:CursosListaComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
