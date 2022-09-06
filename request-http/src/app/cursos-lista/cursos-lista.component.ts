56f33362b3e6a18532f2161920fc449dc4b950d5
Joaocastro20 <joaopedroabreucastro@hotmail.com>
2022-09-06 17:34:07 -0300

    adicionar captura de erro com async

--- request-http/src/app/cursos-lista/cursos-lista.component.ts
+++ request-http/src/app/cursos-lista/cursos-lista.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, empty, Observable, Subject } from 'rxjs';
 import { CursosServiceService } from '../cursos-service.service';
 import { Curso } from '../models/curso';
 
@@ -15,16 +15,24 @@ export class CursosListaComponent implements OnInit {
 
   cursos$!: Observable<Curso[]>;
 
+  error$ = new Subject<boolean>();
+
   constructor(private service: CursosServiceService) { }
 
   ngOnInit(): void {
     // this.service.list().subscribe(dados => this.cursos = dados);
-
-    this.cursos$ = this.service.list();
+    this.onRefresh();
   }
 
   onRefresh(){
-
+    this.cursos$ = this.service.list()
+    .pipe(
+      catchError(error => {
+        console.log(error);
+        this.error$.next(true);
+        return empty();
+      })
+    );
   }
 
   onEdit(curso:any){
