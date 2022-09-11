8b53cd251587d638e47a4ad68112f121843cbfe2
Joaocastro20 <joaopedroabreucastro@hotmail.com>
2022-09-11 13:44:24 -0300

    adicionar barra de progresso upload observando event Http

--- request-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ request-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,3 +1,4 @@
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { UploadServiceService } from './upload-service.service';
 
@@ -9,6 +10,8 @@ import { UploadServiceService } from './upload-service.service';
 export class UploadFileComponent implements OnInit {
 
   files!: Set<File>;
+
+  progress = 0;
 
   constructor(
     private service: UploadServiceService
@@ -22,13 +25,20 @@ export class UploadFileComponent implements OnInit {
     // document.getElementById('customFileLabel').innerHTML = selectFiles[0].name;
     this.files = new Set();
     this.files.add(selectFiles[0]);
-    console.log(this.files)
+    this.progress = 0;
   }
 
   onUpload(){
     if(this.files && this.files.size >= 0){
       this.service.upload(this.files,'/api/upload').subscribe(
-        response => console.log('upload success!')
+        (event:any) => {
+          if(event.type === HttpEventType.Response){
+            console.log('upload success!');
+          }else if(event.type === HttpEventType.UploadProgress){
+            const percentualUpload = Math.round((event.loaded * 100) / event.total);
+            this.progress = percentualUpload;
+          }
+        } 
         );
     }
   }
