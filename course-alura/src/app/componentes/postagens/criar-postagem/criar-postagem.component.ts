1ad07228cca97dda0311c9c4da62f0a0fb7c3690
Abreu <joao.abreu@vetta.digital>
2022-11-02 15:29:32 -0300

    Adicionar formulario cadastro postagem

---
+++ course-alura/src/app/componentes/postagens/criar-postagem/criar-postagem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-postagem',
+  templateUrl: './criar-postagem.component.html',
+  styleUrls: ['./criar-postagem.component.css']
+})
+export class CriarPostagemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
