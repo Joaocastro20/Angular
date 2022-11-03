18c0ecf5018e2123adb214eba0923d87c21b535c
Abreu <joao.abreu@vetta.digital>
2022-11-03 08:21:22 -0300

    Adicionar Lista de Postagens

--- course-alura/src/app/componentes/postagens/listar-postagem/listar-postagem.component.ts
+++ course-alura/src/app/componentes/postagens/listar-postagem/listar-postagem.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-postagem',
   templateUrl: './listar-postagem.component.html',
-  styleUrls: ['./listar-postagem.component.css']
+  styleUrls: ['./listar-postagem.component.css'],
 })
 export class ListarPostagemComponent implements OnInit {
+  postagens = [
+    {
+      conteudo: 'I love Angular',
+      autoria: 'Nay',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'I love Angular',
+      autoria: 'Nay',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo: 'I love Angular',
+      autoria: 'Nay',
+      modelo: 'modelo2',
+    }
 
-  constructor() { }
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
