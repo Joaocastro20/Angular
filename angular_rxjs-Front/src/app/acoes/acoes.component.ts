566b92214ffc8af5db0613d8bb19bba9adeb376d
Joao Castro <joaocastro186@gmail.com>
2022-11-20 17:09:51 -0300

    adicionar filter, debounceTime, DistinctUntilChan

--- angular_rxjs-Front/src/app/acoes/acoes.component.ts
+++ angular_rxjs-Front/src/app/acoes/acoes.component.ts
@@ -3,7 +3,9 @@ import { Acoes, AcoesAPI } from './../shared/models/acoes';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { merge, Subscribable, Subscription } from 'rxjs';
-import { map, tap, switchMap } from 'rxjs/operators';
+import { map, tap, switchMap, filter, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
+const TIME = 300;
 
 @Component({
   selector: 'app-acoes',
@@ -14,6 +16,9 @@ export class AcoesComponent {
   acoesInput = new FormControl();
   todasAcoes$ = this.service.getAcoes();
   filtroInput$ = this.acoesInput.valueChanges.pipe(
+    filter((valorDigitado)=>valorDigitado.length >= 3 && valorDigitado.length < 10 || !valorDigitado.length),
+    debounceTime(TIME),
+    distinctUntilChanged(),
     switchMap((valorDigitado)=>this.service.getAcoes(valorDigitado))
   );
   acoes$ = merge(this.todasAcoes$,this.filtroInput$);
