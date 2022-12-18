bfa4baf3e59306577c6aaf81b344970f6c43bfa8
Joao Castro <joaocastro186@gmail.com>
2022-12-18 18:10:44 -0300

    comeco contador palavras

--- testes_automatizados/src/app/shared/components/monkey-type/monkey-type.component.ts
+++ testes_automatizados/src/app/shared/components/monkey-type/monkey-type.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from '../../../../../node_modules/jquery/dist/jquery.min.js';
+declare var $:any
 
 @Component({
   selector: 'app-monkey-type',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monkey-type.component.scss']
 })
 export class MonkeyTypeComponent implements OnInit {
-
+  qtdFrase: string;
   constructor() { }
 
   ngOnInit(): void {
+    $(document).ready(function(){
+       $('h1').css({color:'orange', display:'block'});
+    })
+    this.contadorPalavras();
   }
 
+  contadorPalavras(){
+    $(document).ready(function(){
+      var frase =  $('.frase').text();
+      var tamanhoFrasearray = frase.split(' ').length
+      console.log(tamanhoFrase)
+      var tamanhoFrase = $('#tamanhoFrase')
+      tamanhoFrase.text(tamanhoFrasearray)
+   })
+  }
 }
