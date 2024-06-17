3e4c208f942a52287656ea898b10545d1130fcf6
Joao Castro <joaocastro186@gmail.com>
2024-06-17 18:57:50 -0300

    funcao de ordem superior

--- angular-9/frontend/src/app/views/javascript-funcional/javascript-funcional.component.ts
+++ angular-9/frontend/src/app/views/javascript-funcional/javascript-funcional.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Formulario } from "./javascript-funcional.model";
 
 @Component({
-  selector: 'app-javascript-funcional',
-  templateUrl: './javascript-funcional.component.html',
-  styleUrls: ['./javascript-funcional.component.css']
+  selector: "app-javascript-funcional",
+  templateUrl: "./javascript-funcional.component.html",
+  styleUrls: ["./javascript-funcional.component.css"],
 })
 export class JavascriptFuncionalComponent implements OnInit {
+  formulario: Formulario = {
+    numero1: 0,
+    numero2: 0,
+    resultado: 0,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  calcular(y: any) {
+    return function (x: any) {
+      return function (fn: any) {
+        return fn(y, x);
+      };
+    };
   }
 
+  adicionar(x: number, y: number) {
+    return x + y;
+  }
+  subtrair(x: number, y: number) {
+    return x - y;
+  }
+  mutiplicar(x: number, y: number) {
+    return x * y;
+  }
+  dividir(x: number, y: number) {
+    return x / y;
+  }
+
+  onClickCaucular() {
+    this.formulario.resultado = this.calcular(this.formulario.numero1)(
+      this.formulario.numero2
+    )(this.adicionar);
+  }
 }
---
+++ angular-9/frontend/src/app/views/javascript-funcional/javascript-funcional.model.ts
@@ -0,0 +1,5 @@
+export interface Formulario {
+    numero1: number| null;
+    numero2: number | null;
+    resultado: number | null;
+}
