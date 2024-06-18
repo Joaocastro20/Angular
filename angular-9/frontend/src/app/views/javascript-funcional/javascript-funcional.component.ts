39e68230131ea2e0c2a811789fd9a552092b542d
Joao Castro <joaocastro186@gmail.com>
2024-06-18 11:51:08 -0300

    add read file

--- angular-9/frontend/src/app/views/javascript-funcional/javascript-funcional.component.ts
+++ angular-9/frontend/src/app/views/javascript-funcional/javascript-funcional.component.ts
@@ -13,6 +13,9 @@ export class JavascriptFuncionalComponent implements OnInit {
     resultado: 0,
   };
 
+  arrowFuc?: string;
+  fileContent: any= null
+
   constructor() {}
 
   ngOnInit(): void {}
@@ -42,5 +45,52 @@ export class JavascriptFuncionalComponent implements OnInit {
     this.formulario.resultado = this.calcular(this.formulario.numero1)(
       this.formulario.numero2
     )(this.adicionar);
+  }
+  //========================================================================================
+  onArrowFunction() {
+    const teste = (teste?: string) => (this.arrowFuc = ` -> ${2 * 2}`);
+    teste();
+
+    //   Array.prototype.ultimo = function () {
+    //     console.log(this[this.length - 1]);
+    // }
+    // const numeros = [1, 2, 3, 4, 5];
+    // numeros.ultimo();
+  }
+
+  // =========================================================================================
+
+  protected exec(fn: any, a: number | null, b: number | null) {
+    return fn(a, b);
+  }
+
+  onExecSomar() {
+    this.formulario.resultado = this.exec(
+      this.adicionar,
+      this.formulario.numero1,
+      this.formulario.numero2
+    );
+  }
+
+  onExecSub() {
+    this.formulario.resultado = this.exec(
+      this.subtrair,
+      this.formulario.numero1,
+      this.formulario.numero2
+    );
+  }
+  // =========================================================================================
+  onFileSelected(event: Event){
+    const input = event.target as HTMLInputElement;
+    if(!input.files?.length){
+      return;
+    }
+    const file = input.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.fileContent = e.target?.result;
+    };
+
+    reader.readAsText(file)
   }
 }
